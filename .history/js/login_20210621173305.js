ff59a8df2892fd2a4dd26923e09660fb64bd1ad3
Alin <1411687596@qq.com>
2021-06-22 18:25:17 +0800

    一键整理，Shift+Alt+F，增加了代码行数，更加美观

---
+++ .history/js/login_20210621173305.js
@@ -0,0 +1,13 @@
+var msglg = function(){
+    $("#msgl").css("border-bottom","3px solid #fcfcfc");
+    $("#pwdl").css("border","none");
+    $(".pwd_form").css("display","none");
+    $(".msg_form").css("display","block");
+}
+
+var pwdlg = function(){
+    $("#pwdl").css("border-bottom","3px solid #fcfcfc");
+    $("#msgl").css("border","none");
+    $(".msg_form").css("display","none");
+    $(".pwd_form").css("display","block");
+}
